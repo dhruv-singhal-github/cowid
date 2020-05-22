8414479448a3bbc30b59cf8d7b6828f92d0dfbce
Dhruv Singhal <drusn003@gmail.com>
2020-05-23 00:54:28 +0530

    card layout updated

--- app/src/main/java/com/example/cowid/MainActivity.java
+++ app/src/main/java/com/example/cowid/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.cowid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.skyfishjy.library.RippleBackground;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+        ImageView imageView=(ImageView)findViewById(R.id.centerImage);
+        rippleBackground.startRippleAnimation();
     }
 }
