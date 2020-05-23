ec91b7bca0fbf01daf4512620096b4076ff56993
Hari-99 <42217251+Hari-99@users.noreply.github.com>
2020-05-23 14:49:52 +0530

    UI_1 (#3)
    * RecyclerView added for HomePage

--- app/src/main/java/com/example/cowid/MainActivity.java
+++ app/src/main/java/com/example/cowid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cowid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Window;
@@ -11,6 +13,10 @@ import com.skyfishjy.library.RippleBackground;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    String[] states = {"Maharashtra", "Delhi", "Odisha", "West Bengal","A&N Islands","Tamil Nadu","Punjab"};
+    String[] cases = {"250","150","100","-1","0","25","100"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,8 @@ public class MainActivity extends AppCompatActivity {
         final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
         ImageView imageView=(ImageView)findViewById(R.id.centerImage);
         rippleBackground.startRippleAnimation();
+        recyclerView = (RecyclerView) findViewById(R.id.statesRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new Adapter(this,states,cases));
     }
 }
